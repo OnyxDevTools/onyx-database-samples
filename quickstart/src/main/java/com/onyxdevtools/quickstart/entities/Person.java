d51907277f4594ad41ce6d6878ab97b310a8b1df
Chris Osborn <c.osborn.contact@gmail>
2015-08-11 12:28:01 -0700

    a

--- quickstart/src/main/java/com/onyxdevtools/quickstart/entities/Person.java
+++ quickstart/src/main/java/com/onyxdevtools/quickstart/entities/Person.java
@@ -1,13 +1,13 @@
 
 package com.onyxdevtools.quickstart.entities;
 
-import com.onyx.persistence.AbstractPersistedEntity;
+import com.onyx.persistence.IPersistedEntity;
 import com.onyx.persistence.annotations.*;
 
 import java.util.Date;
 
 @Entity
-public class Person extends AbstractPersistedEntity
+public class Person implements IPersistedEntity
 {
 
     @Identifier
