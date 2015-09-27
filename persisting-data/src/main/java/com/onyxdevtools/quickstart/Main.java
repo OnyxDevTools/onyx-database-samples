2319e4058c482b990620f58236509bfa02cea13a
Chris Osborn <c.osborn.contact@gmail>
2015-09-26 22:22:56 -0700

    Finished the first persisting-data tutorial

---
+++ persisting-data/src/main/java/com/onyxdevtools/quickstart/Main.java
@@ -0,0 +1,16 @@
+package com.onyxdevtools.quickstart;
+
+import com.onyx.exception.EntityException;
+import com.onyx.exception.InitializationException;
+import java.io.IOException;
+
+public class Main
+{
+
+    public static void main(String[] args) throws InitializationException, EntityException, IOException
+    {
+
+        PersistingDataExample.main(args);
+
+    }
+}
