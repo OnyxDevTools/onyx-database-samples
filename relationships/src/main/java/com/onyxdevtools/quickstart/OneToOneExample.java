43688aeb11af9b5721e8734c0e8d0efa5ab6dd49
Chris Osborn <c.osborn.contact@gmail>
2015-10-21 00:22:01 -0700

    Added relationships as a sub module to it's parent aggregator pom. Also, removed unused imports for the OneToOne example

--- relationships/src/main/java/com/onyxdevtools/quickstart/OneToOneExample.java
+++ relationships/src/main/java/com/onyxdevtools/quickstart/OneToOneExample.java
@@ -2,13 +2,6 @@ package com.onyxdevtools.quickstart;
 
 import com.onyx.exception.EntityException;
 import com.onyx.exception.InitializationException;
-import com.onyx.persistence.factory.impl.CacheManagerFactory;
-import com.onyx.persistence.manager.PersistenceManager;
-import com.onyx.persistence.query.Query;
-import com.onyx.persistence.query.QueryCriteria;
-import com.onyx.persistence.query.QueryCriteriaOperator;
-import com.onyxdevtools.quickstart.entities.Person;
-import java.util.List;
 
 public class OneToOneExample
 {
